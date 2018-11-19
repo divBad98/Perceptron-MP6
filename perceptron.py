5eb50b22de35e439bb0ab6f6d4fd4e48c1426e1d
divBad98 <dbadri2@illinois.edu>
2018-11-19 15:39:02 -0600

    wrote EC nearest neighbor class

--- perceptron.py
+++ perceptron.py
@@ -12,8 +12,36 @@ This is the main entry point for MP6. You should only modify code
 within this file -- the unrevised staff files will be used for all other
 files and classes when code is run, so be careful to not modify anything else.
 """
+import numpy as np
 
-def classify(train_set, train_labels, dev_set, learning_rate,max_iter):
+"""
+This may be handy for extra credit
+Source: Karpathy notes
+"""
+class NearestNeighbor(object):
+    def _init_(self):
+        pass
+    def train(self, X, y):
+        self.Xtr = X
+        self.ytr = y
+    def predict_L1(self, X):
+        num_test = X.shape[0]
+        Ypred = np.zeros(num_test, dtype = self.ytr.dtype)
+        for i in xrange(num_test):
+            distances = np.sum(np.abs(self.Xtr - X[i,:]), axis = 1)
+            min_index = np.argmin(distances)
+            Ypred[i] = self.ytr[min_index]
+        return Ypred
+    def predict_L2(self, X):
+        num_test = X.shape[0]
+        Ypred = np.zeros(num_test, dtype = self.ytr.dtype)
+        for i in xrange(num_test):
+            distances = np.sqrt(np.sum(np.square(self.Xtr - X[i,:]), axis = 1))
+            min_index = np.argmin(distances)
+            Ypred[i] = self.ytr[min_index]
+        return Ypred
+
+def classify(train_set, train_labels, dev_set, learning_rate, max_iter):
     """
     train_set - A Numpy array of 32x32x3 images of shape [7500, 3072].
                 This can be thought of as a list of 7500 vectors that are each
@@ -30,9 +58,15 @@ def classify(train_set, train_labels, dev_set, learning_rate,max_iter):
               It is the same format as train_set
     """
     # TODO: Write your code here
+    train_set_results = {}
+    for i in range(train_set.shape[0]):
+        if (train_labels[i] == 1):
+            break
+        else:
+            break
     # return predicted labels of development set
     return []
 
-def classifyEC(train_set, train_labels, dev_set,learning_rate,max_iter):
+def classifyEC(train_set, train_labels, dev_set,learning_rate, max_iter):
     # Write your code here if you would like to attempt the extra credit
     return []
